6dbcc0a90dd8390939836d5b6eb9561cc8324eb5
javad270 <javad.eslami73@gmail.com>
2024-09-01 00:49:33 +0200

    cleanup builder inheritance pattern

---
+++ BuilderInheritance/PersonBirthDateBuilder.cs
@@ -0,0 +1,16 @@
+﻿namespace BuilderInheritance
+{
+  // here's another inheritance level
+  // note there's no PersonInfoBuilder<PersonJobBuilder<PersonBirthDateBuilder<SELF>>>!
+
+  public class PersonBirthDateBuilder<SELF> 
+    : PersonJobBuilder<PersonBirthDateBuilder<SELF>>
+    where SELF : PersonBirthDateBuilder<SELF>
+  {
+    public SELF Born(DateTime dateOfBirth)
+    {
+      person.DateOfBirth = dateOfBirth;
+      return (SELF)this;
+    }
+  }
+}
